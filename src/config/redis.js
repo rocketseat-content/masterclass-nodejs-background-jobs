99bf6247dc5782373c38bdfdcb0213f8a26b458c
Hunglmc <39206819+Hunglmc@users.noreply.github.com>
2024-04-08 02:35:13 +0000

    changre

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,7 +1,5 @@
 export default {
-  //host: process.env.REDIS_HOST,
-  ///port: process.env.REDIS_PORT,
-    password: 'hZ9mwcPkSP9eopimMtBPrChyiNbRKgT1',
-    host: 'redis-17776.c14.us-east-1-2.ec2.cloud.redislabs.com',
-    port: 17776
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password:process.env.REDIS_PASSWORD
 };
